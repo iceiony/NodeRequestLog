6feeee82d3b89e74b9628ea9536cf71829e5fe04
adrian <iceiony@yahoo.com>
2012-12-05 21:33:07 +0000

    remove redundant variable

--- src/FormatOutput.ts
+++ src/FormatOutput.ts
@@ -1,7 +1,6 @@
 ///<reference path='lib.d.ts' />
 export class SimpleFormatOutput{
     formatJASON(jason: String){
-		var result = jason.replace(/{/g,'{\n').replace(/}/g,'\n}\n').replace(/",/g,'",\n');
-		return result;
-    };
+		return jason.replace(/{/g,'{\n').replace(/}/g,'\n}\n').replace(/",/g,'",\n');
+	};
 }
