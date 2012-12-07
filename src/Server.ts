22542ed01dff5be361cb3de6a2e484f17cd2d02e
adrian <iceiony@yahoo.com>
2012-12-07 22:16:13 +0000

    Convert server code from plain js to proper ts class
    this will get rid of typescript weird compile problems

--- src/Server.ts
+++ src/Server.ts
@@ -6,45 +6,66 @@ import http = module('http');
 import mongo = module('mongodb');
 import format = module('./FormatOutput');
 
-console.log('server starting');
+class ServerWrapper {
+    formatter:format.SimpleFormatOutput;
+    mongo_server_config:mongo.Server;
+    dataCollection:mongo.Collection;
 
-var formatter = new format.SimpleFormatOutput(),
-        mongo_server_config = new mongo.Server("127.0.0.1", 27017, {}),
-        handleResponse = function (req, res) {
-            res.writeHead(200, {
-                "Content-Type": "text/plain"
-            });
+    constructor() {
+        console.log("Initialising...")
 
-            res.write(req.method + '\n');
-            res.write(req.url + '\n');
-            res.write(formatter.formatJASON(JSON.stringify(req.headers)));
+        var instance = this;
+        var mongo_server_config = new mongo.Server("127.0.0.1", 27017, {});
 
-            req.on('data', function (data) {
-                res.write(data.toString());
-            });
-            res.end();
-        },
-        recordRequest = (function () {
-            var dataCollection: mongo.Collection = null;
-            new mongo.Db('RequestLog', mongo_server_config, {w: 1}).open(function (error, client) {
-                if (error) throw error;
-                dataCollection = new mongo.Collection(client, 'requestData');
-            });
-            return function (req) {
-                console.log("Saving request");
-                dataCollection.insert({
-                    method: req.method,
-                    url: req.url,
-                    headers: req.headers
-                }, {safe: false});
-                console.log("Request saved");
-            };
-        })(),
-        server = http.createServer(function (req, res) {
-            handleResponse(req, res);
-            recordRequest(req);
+        this.formatter = new format.SimpleFormatOutput();
+        this.dataCollection = null;
+
+        new mongo.Db('RequestLog', mongo_server_config, {w: 1}).open(function (error, client) {
+            if (error) throw error;
+            instance.dataCollection = new mongo.Collection(client, 'requestData');
+        });
+    }
+
+    public handleResponse(req: http.ServerRequest, res: http.ServerResponse) {
+        res.writeHead(200, {
+            "Content-Type": "text/plain"
         });
 
+        res.write(req.method + '\n');
+        res.write(req.connection.remoteAddress + '\n');
+        res.write(req.url + '\n');
+        res.write(this.formatter.formatJASON(JSON.stringify(req.headers)));
 
-server.listen(8000);
-console.log('Server running on port 8000');
+        req.on('data', function (data) {
+            res.write(data.toString());
+        });
+        res.end();
+    }
+
+    public recordRequest(req: http.ServerRequest) {
+        console.log("Saving request");
+        this.dataCollection.insert({
+            method: req.method,
+            url: req.url,
+            ip: req.connection.remoteAddress,
+            headers: req.headers
+        }, {safe: false});
+        console.log("Request saved");
+    }
+
+    public start() {
+        console.log('Server starting');
+
+        var instance = this;
+        var server = http.createServer(function (req, res) {
+            instance.handleResponse(req, res);
+            instance.recordRequest(req);
+        });
+        server.listen(8000);
+
+        console.log('Server running on port 8000');
+    }
+}
+
+var localServer: ServerWrapper = new ServerWrapper();
+localServer.start();
